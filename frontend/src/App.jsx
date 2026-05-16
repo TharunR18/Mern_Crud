35a22bdc8fabc2f04229e13e393a55c613edf725
TharunR18 <its.tharun018@gmail.com>
2026-05-16 21:01:51 +0530

    ...typo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 
 function App() {
